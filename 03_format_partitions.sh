3923a5fb20cd7b607eb1b856ef05375c81061418
Chris-F5 <christopher-d-lang@outlook.com>
2021-07-22 20:14:25 +0100

    Force formating

--- 03_format_partitions.sh
+++ 03_format_partitions.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # Format EFI partition
-mkfs.fat -F32 /dev/sda1
+mkfs.fat -F -F32 /dev/sda1
 
 # Format swap partition
 mkswap /dev/sda2
 swapon /dev/sda2
 
 # Format root partition
-mkfs.ext4 /dev/sda3
+mkfs.ext4 -F /dev/sda3
