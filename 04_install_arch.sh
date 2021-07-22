f5c4443e0322fb58d20ff1ad9e938becc8f5bb4a
Chris-F5 <christopher-d-lang@outlook.com>
2021-07-22 20:37:05 +0100

    Fix OPTIONAL_PACKAGES varable declaration

--- 04_install_arch.sh
+++ 04_install_arch.sh
@@ -3,7 +3,7 @@
 mount /dev/sda3 /mnt
 
 # Install arch linux
-OPTIONAL_PACKAGES = "vim"
+OPTIONAL_PACKAGES="vim"
 pacstrap /mnt base linux linux-firmware $OPTIONAL_PACKAGES
 
 # Generate file system table
